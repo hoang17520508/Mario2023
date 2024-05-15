5f11d29b95938633b6f64dbc0fdd8f11fa31bf87
Hoanguitk12 <luna02091999@gmail.com>
2024-05-16 02:55:58 +0700

    [ADD] hth add question brick class, question brick data

---
+++ 05-SceneManager/QuestionBrick.cpp
@@ -0,0 +1,16 @@
+#include "QuestionBrick.h"
+
+void CQuestionBrick::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+	animations->Get(ID_ANI_QUESTION_BRICK)->Render(x, y);
+	//RenderBoundingBox();
+}
+
+void CQuestionBrick::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - QUESTION_BRICK_BBOX_WIDTH / 2;
+	t = y - QUESTION_BRICK_BBOX_HEIGHT / 2;
+	r = l + QUESTION_BRICK_BBOX_WIDTH;
+	b = t + QUESTION_BRICK_BBOX_HEIGHT;
+}
